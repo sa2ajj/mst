9c6272a5a3fe8bc121e4ae704e0ae08970bb569b
Mikhail Sobolev <mss@mawhrin.net>
2005-07-29 06:38:50 +0000

    added c++ stuff; reformatting

--- util.h
+++ util.h
@@ -1,20 +1,18 @@
 #ifndef _HAVE_UTIL_HEADER
 #define _HAVE_UTIL_HEADER
 
-char *
-util_mprintf(const char *format, ...);
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int 
-util_ascii_to_hex(int c);
+  char *util_mprintf(const char *format, ...);
+  int util_ascii_to_hex(int c);
+  void util_dehttpize(char *z);
+  char *util_extract_token(char *input, char **leftOver);
+  char *util_htmlize(const char *in, int n);
 
-void 
-util_dehttpize(char *z);
-
-char *
-util_extract_token(char *input, char **leftOver);
-
-char *
-util_htmlize(const char *in, int n);
-
+#ifdef __cplusplus
+};
+#endif
 
 #endif
