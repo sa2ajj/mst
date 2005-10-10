e6cdc424cc25b9354f6b5939ea05c5e8a7b09714
Mikhail Sobolev <mss@mawhrin.net>
2005-10-10 19:51:22 +0000

    * a really bad commit (re-formatting together with meaningful changes)  * do not try to save picture in case root_window or corresponding pixbuf are not available

--- screenshot.c
+++ screenshot.c
@@ -1,28 +1,38 @@
+#include <stdio.h>
+
 #include <gtk/gtk.h>
 
 #include "screenshot.h"
 
 gboolean get_screenshot(GdkPixbufSaveFunc save_func, gpointer data, GError **error) {
-  gboolean ret = FALSE;
+	gboolean ret = FALSE;
 
-  GdkDrawable *root_window;
-  int          width, height;
-  GdkPixbuf   *pixbuf;
+	GdkDrawable *root_window;
+	int width, height;
+	GdkPixbuf *pixbuf;
 
-  root_window = gdk_get_default_root_window();
-  gdk_drawable_get_size(root_window, &width, &height);
+	root_window = gdk_get_default_root_window();
 
-  pixbuf = gdk_pixbuf_get_from_drawable(NULL,
-              root_window,
-              gdk_drawable_get_colormap(root_window),
-              0, 0,
-              0, 0,
-              width, height);
+	if (root_window != 0) {
+		gdk_drawable_get_size(root_window, &width, &height);
 
+		pixbuf = gdk_pixbuf_get_from_drawable(NULL,
+							root_window,
+							gdk_drawable_get_colormap(root_window),
+							0, 0,
+							0, 0,
+							width, height);
 
-  ret = gdk_pixbuf_save_to_callback(pixbuf, save_func, data, "png", error, NULL);
+		if (pixbuf != 0) {
+			ret = gdk_pixbuf_save_to_callback(pixbuf, save_func, data, "png", error, NULL);
 
-  g_object_unref (pixbuf);
+			g_object_unref (pixbuf);
+		} else {
+			fprintf(stderr, "Unable to get pixbuf\n");
+		}
+	} else {
+		fprintf(stderr, "Unable to get root window\n");
+	}
 
-  return ret;
+	return ret;
 }
